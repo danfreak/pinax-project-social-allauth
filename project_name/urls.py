e5c11e55ba660ebedfe8fc525dcc53ba6dbfa38c
danfreak <dan@freakclimbing.com>
2013-03-25 19:38:29 +0100

    template and urls changes to make it compatible with django 1.5

--- project_name/urls.py
+++ project_name/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -11,12 +11,12 @@ from .views import SignupView
 
 
 urlpatterns = patterns("",
-    url(r"^$", direct_to_template, {"template": "homepage.html"}, name="home"),
+    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
     #url(r"^account/signup/$", SignupView.as_view(), name="account_signup"),
     url(r"^account/", include("account.urls")),
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^accounts/profile/$', 'django.views.generic.simple.direct_to_template', {'template': 'profile.html' }),
+    url(r'^accounts/profile/$', TemplateView.as_view(template_name="profile.html" )),
 )
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
